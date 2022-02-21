b48742d52eab0b41a16e8988f868584275d43103
rizarizkan <rizkan.forever@gmail.com>
2022-02-21 15:09:32 +0700

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -2,9 +2,13 @@
 
 cd /var/www
 
-chmod -R 777 storage
+chown -R www:www-data storage/logs
+chmod -R 777 storage/logs/laravel.log
+
 # php artisan migrate:fresh --seed
 php artisan cache:clear
 php artisan route:cache
 
 /usr/bin/supervisord -c /etc/supervisord.conf
+chown -R www:www-data storage/logs
+chmod -R 777 storage/logs/laravel.log
