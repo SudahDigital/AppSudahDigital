7417bf1663ceaa22caf1a9015bc824c99942eddf
rizarizkan <rizkan.forever@gmail.com>
2022-02-21 12:04:23 +0700

    Update run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -2,8 +2,6 @@
 
 cd /var/www
 
-RUN curl --header "X-Consul-Token:${env.CONSUL_TOKEN}" \
--XGET https://consul.sudahdigital.com/v1/kv/dev/apptest.sudahdigital.com?raw=true > .env
 
 # php artisan migrate:fresh --seed
 php artisan cache:clear
