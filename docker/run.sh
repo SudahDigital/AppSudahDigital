b58d29fab4e99a5d171d1d2f458267d4e5aca9ad
Zuki Setiawan <setyawanzuky@gmail.com>
2022-02-23 16:02:30 +0700

    update run sh

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,7 @@ cd /var/www
 php artisan cache:clear
 php artisan route:cache
 #php artisan migrate:fresh
-php artisan migrate:rollback
+php artisan migrate
 
 /usr/bin/supervisord -c /etc/supervisord.conf
 chown -R root:root storage/
