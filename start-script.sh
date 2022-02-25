23b2d3cfdacca75b9f2e4b7bb2b80877b395b4bd
rizarizkan <rizkan.forever@gmail.com>
2022-02-25 23:19:39 +0700

    Update start-script.sh

--- start-script.sh
+++ start-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-s3fs $S3_BUCKET_NAME $S3_MOUNT_DIRECTORY -o nonempty -o passwd_file=/root/.passwd-s3fs
+s3fs sudahdigital /var/www/storage/app/public -o nonempty -o passwd_file=/root/.passwd-s3fs
